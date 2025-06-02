9bd0aa68eea822450563bb4bfd204f4fbf24b59f
Yash_KB <123755515+yashBorkar404@users.noreply.github.com>
2025-06-03 00:02:14 +0530

    Update calendar.tsx

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -57,7 +57,7 @@ function Calendar({
         IconLeft: (_props) => <ChevronLeft className="h-4 w-4" />,
         IconRight: (_props) => <ChevronRight className="h-4 w-4" />,
       }}
-      {..._props}
+      {...props}
     />
   )
 }
